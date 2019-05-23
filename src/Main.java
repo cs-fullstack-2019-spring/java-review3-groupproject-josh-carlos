83d5cc5f381c69e4a25d0924dc2c05d3a5e517ba
carlos <cgarciapieto@gmail.com>
2019-05-23 14:53:54 -0500

    firstPush

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 import myClasses_Functions.*;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+//        Scanner newUser = new Scanner();
+        CreateUser x = new CreateUser();
+        System.out.println(x);
+
 
         System.out.println("Hello World!");
         message blank = new message();
